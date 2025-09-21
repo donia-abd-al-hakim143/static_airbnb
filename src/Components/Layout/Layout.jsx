d742e9f7d9d80357a45653e07fd56016f56b13d0
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-09-21 21:48:42 +0300

    Add page layout

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
-  return (
-    <div>
-      layout
-    </div>
-  )
+    return (
+        <div>
+            
+            {/* call navbar component here */}
+            <Outlet />
+            {/* call footer component here */}
+
+        </div>
+    )
 }
