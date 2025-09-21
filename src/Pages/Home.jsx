c44b53b64c8ac5a34d60d6b2b8fdeed1a9ec7f56
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-09-21 23:54:42 +0300

    get data and show it at home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { homeData } from "../Data/HomeData";
+import Card from "../Components/Card/Card";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="container my-5">
+      {homeData.map((city, index) => (
+        <div key={index} className="mb-5">
+          <h3 className="mb-4">{city.cityName}</h3>
+          <div className="row">
+            {city.details.map((detail, i) => (
+              <Card key={i} detail={detail} />
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
